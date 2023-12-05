256f2a6dd95f8fb4afa55a489b3aae44370d7a80
Yurii Kazakov <gfnvortex@gmail.com>
2023-12-05 12:23:30 +0200

    start

--- src/components/ToDo/TodoApp.js
+++ src/components/ToDo/TodoApp.js
@@ -1,156 +1,5 @@
-//
-// // src/components/ToDo/TodoApp.js
-// import React, { useState, useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-//
-// import {
-//     addTodo,
-//     fetchTodos,
-//     selectAllTodos,
-//     deleteSelectedTodosLocally,
-//     removeTodo,
-// } from '../../store/slices/todoSlice';
-//
-// import Todo from './Todo';
-//
-// const TodoApp = () => {
-//     const dispatch = useDispatch();
-//
-//     const { loading, error, todoArray } = useSelector((state) => state.todos);
-//
-//     const [newTodo, setNewTodo] = useState('');
-//     const [selectedTodos, setSelectedTodos] = useState([]);
-//     const [selectAll, setSelectAll] = useState(false);
-//     const [currentPage, setCurrentPage] = useState(1);
-//     const todosPerPage = 10;
-//
-//     useEffect(() => {
-//         dispatch(fetchTodos());
-//     }, [dispatch]);
-//
-//     const addNewTodo = () => {
-//         if (newTodo.trim() !== '') {
-//             const newTodoObject = {
-//                 title: newTodo,
-//                 completed: false,
-//                 selected: false,
-//                 local: true,
-//                 id: Date.now().toString(),
-//             };
-//
-//             dispatch(addTodo(newTodoObject));
-//             setNewTodo('');
-//         }
-//     };
-//
-//     const handleSelectAll = () => {
-//         const allTodoIds = todoArray.map((todo) => todo.id);
-//
-//         if (!selectAll) {
-//             setSelectedTodos(allTodoIds);
-//         } else {
-//             setSelectedTodos([]);
-//         }
-//
-//         setSelectAll((prev) => !prev);
-//         dispatch(selectAllTodos());
-//     };
-//
-//     const toggleSelectedTodo = (todo) => {
-//         setSelectedTodos((prevSelectedTodos) => {
-//             if (prevSelectedTodos.includes(todo.id)) {
-//                 return prevSelectedTodos.filter((id) => id !== todo.id);
-//             } else {
-//                 return [...prevSelectedTodos, todo.id];
-//             }
-//         });
-//
-//         dispatch(selectAllTodos());
-//     };
-//
-//     const handleDeleteSelected = () => {
-//         // Разделяем тудушки на локальные и полученные из API
-//         const localTodos = selectedTodos.filter((id) => {
-//             const todo = todoArray.find((t) => t.id === id);
-//             return todo && todo.local;
-//         });
-//
-//         const apiTodos = selectedTodos.filter((id) => !localTodos.includes(id));
-//
-//         // Удаляем локальные тудушки
-//         localTodos.forEach((id) => {
-//             dispatch(removeTodo(id));
-//         });
-//
-//         // Удаляем полученные из API тудушки
-//         if (apiTodos.length > 0) {
-//             dispatch(deleteSelectedTodosLocally(apiTodos));
-//         }
-//
-//         setSelectedTodos([]);
-//         dispatch(selectAllTodos());
-//     };
-//
-//     const indexOfLastTodo = currentPage * todosPerPage;
-//     const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
-//     const currentTodos = todoArray.slice(indexOfFirstTodo, indexOfLastTodo);
-//
-//     const totalPages = Math.ceil(todoArray.length / todosPerPage);
-//
-//     const handlePageChange = (page) => {
-//         setCurrentPage(page);
-//     };
-//
-//     return (
-//         <div>
-//             <h1>Todo List</h1>
-//             <input type="checkbox" checked={selectAll} onChange={handleSelectAll} />
-//             <input
-//                 type="text"
-//                 value={newTodo}
-//                 onChange={(e) => setNewTodo(e.target.value)}
-//                 placeholder="Enter a new todo"
-//             />
-//             <button onClick={addNewTodo}>Add Todo</button>
-//             <button onClick={handleDeleteSelected}>Delete Selected</button>
-//
-//             {loading && <h3>Loading....</h3>}
-//             {error && <h3>{error}</h3>}
-//
-//             <ul
-//                 style={{
-//                     marginTop: '20px',
-//                     display: 'flex',
-//                     flexDirection: 'column',
-//                     alignItems: 'flex-start',
-//                     justifyContent: 'center',
-//                 }}
-//             >
-//                 {currentTodos.map((todo) => (
-//                     <Todo
-//                         key={todo.id}
-//                         todo={todo}
-//                         isSelected={selectedTodos.includes(todo.id)}
-//                         toggleSelected={() => toggleSelectedTodo(todo)}
-//                     />
-//                 ))}
-//             </ul>
-//
-//             <div className='page_numbers' style={{ marginTop: '5px' }}>
-//                 {Array.from({ length: totalPages }, (_, index) => (
-//                     <button key={index + 1} onClick={() => handlePageChange(index + 1)}>
-//                         {index + 1}
-//                     </button>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default TodoApp;
 
 // src/components/ToDo/TodoApp.js
-
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -220,6 +69,7 @@ const TodoApp = () => {
     };
 
     const handleDeleteSelected = () => {
+        // Разделяем тудушки на локальные и полученные из API
         const localTodos = selectedTodos.filter((id) => {
             const todo = todoArray.find((t) => t.id === id);
             return todo && todo.local;
@@ -227,10 +77,12 @@ const TodoApp = () => {
 
         const apiTodos = selectedTodos.filter((id) => !localTodos.includes(id));
 
+        // Удаляем локальные тудушки
         localTodos.forEach((id) => {
             dispatch(removeTodo(id));
         });
 
+        // Удаляем полученные из API тудушки
         if (apiTodos.length > 0) {
             dispatch(deleteSelectedTodosLocally(apiTodos));
         }
@@ -251,13 +103,13 @@ const TodoApp = () => {
 
     return (
         <div>
-            <h1 style={{ textAlign: 'center', marginBottom: '0.5em' }}>Todo List</h1>
-            <input type='checkbox' checked={selectAll} onChange={handleSelectAll} />
+            <h1>Todo List</h1>
+            <input type="checkbox" checked={selectAll} onChange={handleSelectAll} />
             <input
-                type='text'
+                type="text"
                 value={newTodo}
                 onChange={(e) => setNewTodo(e.target.value)}
-                placeholder='Enter a new todo'
+                placeholder="Enter a new todo"
             />
             <button onClick={addNewTodo}>Add Todo</button>
             <button onClick={handleDeleteSelected}>Delete Selected</button>
@@ -296,3 +148,4 @@ const TodoApp = () => {
 };
 
 export default TodoApp;
+
